b4bcf953993c89053bb3bd06e50cd332ea06fad3
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-12-21 14:08:20 -0300

    feat: Add page Home about structure

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import './Home.module.css';
+import styles from './Home.module.css';
 
 class Home extends React.Component {
   render () {
     return (
       <main>
-        <section>
-        <div>
+        <section className={ styles.sectionApresentation }>
+        <div className={ styles.apresentation }>
           <p>Olá, eu sou
           <h1>GEFFERSON<br/>BATISTA</h1>
           e estudo desenvolvimento web full-stack na Trybe.</p>
         </div>
-        <div>
+        <div className={ styles.apresentationPhoto }>
           <img src="" alt="foto de perfil" />
+        </div>
+        </section>
+        <section className={ styles.sectionAbout }>
+        <div className={ styles.helloWorld }>
+          <h1>HELLO,<br/>WORLD</h1>
+        </div>
+        <div className={ styles.aboutDescription }>
+          <h1>About</h1>
+          <p>Description about me</p>
         </div>
         </section>
       </main>
