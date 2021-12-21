65646942e91c04beaf3dcc260f687c8ce637ff16
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-12-21 00:01:20 -0300

    feat: Add page Home apresentation structure

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './Home.module.css';
+
+class Home extends React.Component {
+  render () {
+    return (
+      <main>
+        <section>
+        <div>
+          <p>Olá, eu sou
+          <h1>GEFFERSON<br/>BATISTA</h1>
+          e estudo desenvolvimento web full-stack na Trybe.</p>
+        </div>
+        <div>
+          <img src="" alt="foto de perfil" />
+        </div>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default Home;
